9ffc9b77b44244f55dce6e2922c871db9bbcb368
ryuto <tun_tnd@i.softbank.jp>
2016-01-08 09:21:54 +0900

    Add password resets & email configuration

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,13 +1,19 @@
-class UserMailer < ApplicationMailer
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
 
-  def account_activation(user)
-    @user = user
-    mail to: user.email, subject: "Account activation"
+  # Preview this email at
+  # http://localhost:3000/rails/mailers/user_mailer/account_activation
+  def account_activation
+    user = User.first
+    user.activation_token = User.new_token
+    UserMailer.account_activation(user)
   end
 
+  # Preview this email at
+  # http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 end
