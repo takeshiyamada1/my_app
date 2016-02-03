45d3058b9babfa52b74f364294685b3a5cf81395
ryuto <tun_tnd@i.softbank.jp>
2016-02-03 11:19:51 +0900

    Second time of fixed

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,7 +19,7 @@ module SessionsHelper
 
   #記憶トークンcookieに対応するユーザーを返す
   def current_user
-    if (user_id = session[:user_id])
+    if  (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ActiveRecord::Base
- has_many :microposts, dependent: :destroy
- has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id",dependent: :destroy
- has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id",dependent: :destroy
- has_many :following, through: :active_relationships, source: :followed
- has_many :followers, through: :passive_relationships, source: :follower
- attr_accessor :remember_token, :activation_token, :reset_token
- before_save :downcase_email
- before_create :create_activation_digest
- validates :name, presence: true, length: { maximum: 50 }
- VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
- validates :email, presence: true, length: { maximum: 255 },
- format: { with: VALID_EMAIL_REGEX },
- uniqueness: { case_sensitive: false }
- has_secure_password
- validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id",dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id",dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save :downcase_email
+  before_create :create_activation_digest
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+  format: { with: VALID_EMAIL_REGEX },
+  uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   #与えられた文字列のハッシュ値を返す
   def User.digest(string)
@@ -40,12 +40,12 @@ class User < ActiveRecord::Base
     BCrypt::Password.new(digest).is_password?(token)
   end
 
- 	#Userログインを破棄する
+  #Userログインを破棄する
   def forget
     update_attribute(:remember_digest, nil)
   end
 
- 	#アカウントを有効にする
+  #アカウントを有効にする
   def activate 
     update_attribute(:activated, true)
     update_attribute(:activated_at, Time.zone.now)
@@ -56,7 +56,7 @@ class User < ActiveRecord::Base
     UserMailer.account_activation(self).deliver_now
   end
 
- 	 # パスワード再設定の属性を設定する
+   # パスワード再設定の属性を設定する
   def create_reset_digest
     self.reset_token = User.new_token
     update_attribute(:reset_digest,  User.digest(reset_token))
