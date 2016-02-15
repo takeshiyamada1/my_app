e619a611d900b525464848b99a684b7c8e7ff818
ryuto <tun_tnd@i.softbank.jp>
2016-02-15 19:03:36 +0900

    fixes#17Finish change rspec

--- Gemfile
+++ Gemfile
@@ -28,12 +28,7 @@ group :development, :test do
   gem 'web-console', '2.0.0.beta3'
   gem 'spring',      '1.1.3'
   gem 'factory_girl_rails', '~> 4.5.0'
-end
-
-group :test do
-  gem 'minitest-reporters', '1.0.5'
-  gem 'mini_backtrace',     '0.1.3'
-  gem 'guard-minitest',     '2.3.1'
+  gem 'rspec-rails', '3.4.0'
 end
 
 group :production do
---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Micropost, type: :models do
+  let(:user) { create :tsubasa }
+  let(:micropost) { user.microposts.build(content: "Lorem ipsum") }
+
+  it "should be valid" do
+    expect(micropost).to be_valid
+  end
+
+  it "user id should be present" do
+    micropost.user = nil
+    expect(micropost).to_not be_valid
+  end
+
+  it "content should be present" do
+    micropost.content = ' '
+    expect(micropost).to_not be_valid
+  end
+
+  it "content should be at most 140 characters" do
+    micropost.content = 'a'*141
+    expect(micropost).to_not be_valid
+  end
+
+  it "order should be most recent first" do
+    microposts = create :most_recent
+    expect(microposts).to eq Micropost.first
+  end
+end
