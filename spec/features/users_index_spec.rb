70f5a61c36ede2b9c457c8c798fcd2d0b7cbf980
ryuto <tun_tnd@i.softbank.jp>
2016-02-23 10:12:02 +0900

    fixes#42#18fixed users index

--- spec/features/users_index_spec.rb
+++ spec/features/users_index_spec.rb
@@ -4,10 +4,10 @@ RSpec.feature "UsersIndex",type: :feature do
   before do
     @admin = create :tsubasa
     @non_admin = create :sayami
+    create_list :user, 30
   end
 
   it "index as admin including pagination and delete links" do |variable|
-    pending('no patch pagination')
     sign_in_as(@admin)
     visit users_path
     expect(page).to have_selector 'h1', text: 'All users'
@@ -16,10 +16,10 @@ RSpec.feature "UsersIndex",type: :feature do
     first_page_of_users.each do |user|
       expect(page).to have_link user.name, href: user_path(user)
       unless user == @admin
-        expect(page).to have_link 'delete', href: user_path
+        expect(page).to have_link 'delete', href: user_path(user)
       end
     end
-    expect { click_link 'delete',href: user_path(@non_admi) }.to change{ User.count }.by(-1)
+    expect { click_link 'delete',href: user_path(@non_admin) }.to change{ User.count }.by(-1)
   end
 
   it "index as non-admin" do
