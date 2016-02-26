2454f044a12477a19a90605b3dc9482de59e3cd4
ryuto <tun_tnd@i.softbank.jp>
2016-02-26 13:31:00 +0900

    fixes#35fixed microposts controller

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -1,25 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe MicropostsController, type: :controller do
-  before do
-    @micropost = create :orange
+
+  context 'create and destroy when not logged in' do
+    before do
+      @micropost = create :orange
+    end 
+
+    it 'should redirect create when not logged in' do
+      expect { post :create, micropost: { content: 'Lorem ipsum' } }.to_not change { Micropost.count }
+      expect(response).to redirect_to login_url
+    end
+
+    it 'should redirect destroy when not logged in' do
+      expect { delete :destroy, id: @micropost }.to_not change { Micropost.count }
+      expect(response).to redirect_to login_url
+    end
   end
 
-  it 'should redirect create when not logged in' do
-    expect { post :create, micropost: { content: 'Lorem ipsum' } }.to_not change { Micropost.count }
-    expect(response).to redirect_to login_url
-  end
+  context 'wrong micropost destroy' do
+    before do
+      @micropost = create :orange
+      user = build :tsubasa
+      log_in_as(user)
+    end
 
-  it 'should redirect destroy when not logged in' do
-    expect { delete :destroy, id: @micropost }.to_not change { Micropost.count }
-    expect(response).to redirect_to login_url
-  end
-
-  it 'should redirect destroy for wrong micropost' do
-    user = create :tsubasa
-    log_in_as(user)
-    ants = create :ants
-    expect { delete :destroy, id: ants }.to_not change { Micropost.count }
-    expect(response).to redirect_to root_url
+    it 'should redirect destroy for wrong micropost' do
+      ants = create :ants
+      expect { delete :destroy, id: ants }.to_not change { Micropost.count }
+      expect(response).to redirect_to root_url
+    end
   end
 end
