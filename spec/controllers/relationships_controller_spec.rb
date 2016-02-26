5080f50f4f5dabd33bea6b0c3b1b2fda2fc88ebd
ryuto <tun_tnd@i.softbank.jp>
2016-02-26 14:44:03 +0900

    fixes#35#36fixed relationship controller

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -1,28 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe RelationshipsController, type: :controller do
-  let(:user) { create :tsubasa }
-  let(:other) { create :sayami }
-  it 'create should require logged-in user' do
-    expect { post :create }.to_not change { Relationship.count }
-    expect(response).to redirect_to login_url
+  context 'create and destroy logged-in' do
+    let!(:one) { create :one }
+    it 'create should require logged-in user' do
+      expect { post :create }.to_not change { Relationship.count }
+      expect(response).to redirect_to login_url
+    end
+
+    it 'destroy should require logged-in' do
+      expect { delete :destroy, id: one }.to_not change { Relationship.count }
+      expect(response).to redirect_to login_url
+    end
   end
 
-  it 'destroy should require logged-in' do
-    one = create :one
-    expect { delete :destroy, id: one }.to_not change { Relationship.count }
-    expect(response).to redirect_to login_url
-  end
+  context 'follow and unfollow with ajax' do
+    let(:user) { create :tsubasa }
+    let(:other) { create :sayami }
+    before do
+      log_in_as(user)
+    end
+    it 'should follow a user with ajax' do
+      expect { xhr :post, :create, followed_id: other.id }.to change { user.following.count }.by(1)
+    end
 
-  it 'should follow a user with ajax' do
-    log_in_as(user)
-    expect { xhr :post, :create, followed_id: other.id }.to change { user.following.count }.by(1)
-  end
-
-  it 'should unfollow a user with ajax' do
-    log_in_as(user)
-    user.follow(other)
-    relationship = user.active_relationships.find_by(followed_id: other.id)
-    expect { xhr :post, :destroy, id: relationship.id }.to change { user.following.count }.by(-1)
+    it 'should unfollow a user with ajax' do
+      user.follow(other)
+      relationship = user.active_relationships.find_by(followed_id: other.id)
+      expect { xhr :post, :destroy, id: relationship.id }.to change { user.following.count }.by(-1)
+    end
   end
 end
