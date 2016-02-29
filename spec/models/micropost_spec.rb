a17beee4d8f75bf996eb7e9d118fa04b67a97fe5
ryuto <tun_tnd@i.softbank.jp>
2016-02-29 11:39:59 +0900

    fixes#35#36fixed micropost spec

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,30 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :models do
-  let(:user) { create :tsubasa }
-  let(:micropost) { user.microposts.build(content: 'Lorem ipsum') }
+  context 'micropost' do
+    let(:user) { create :tsubasa }
+    let(:micropost) { user.microposts.build(content: 'Lorem ipsum') }
 
-  it 'should be valid' do
-    expect(micropost).to be_valid
-  end
+    it 'should be valid' do
+      expect(micropost).to be_valid
+    end
 
-  it 'user id should be present' do
-    micropost.user = nil
-    expect(micropost).to_not be_valid
-  end
+    context 'user id' do
+      before do
+        micropost.user = nil
+      end
+      it 'user id should be present' do
+        expect(micropost).to_not be_valid
+      end
+    end
 
-  it 'content should be present' do
-    micropost.content = ' '
-    expect(micropost).to_not be_valid
-  end
+    context 'content present' do
+      before do
+        micropost.content = ' '
+      end
+      it 'content should be present' do
+        expect(micropost).to_not be_valid
+      end
+    end
+    
+    context 'content most 140 characters' do
+      before do
+        micropost.content = 'a' * 141
+      end
+      it 'content should be at most 140 characters' do
+        expect(micropost).to_not be_valid
+      end
+    end
 
-  it 'content should be at most 140 characters' do
-    micropost.content = 'a' * 141
-    expect(micropost).to_not be_valid
-  end
-
-  it 'order should be most recent first' do
-    microposts = create :most_recent
-    expect(microposts).to eq Micropost.first
+    context 'order' do
+      let(:microposts) { create :most_recent }
+      it 'order should be most recent first' do
+        expect(microposts).to eq Micropost.first
+      end
+    end
   end
 end
