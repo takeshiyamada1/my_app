754c12c05abddc72ed8de7f8244b03c1688ce65e
ryuto <tun_tnd@i.softbank.jp>
2016-02-29 18:20:11 +0900

    fixes#35#36fixed users profile spec

--- spec/features/users_profile_spec.rb
+++ spec/features/users_profile_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature 'UsersProfile', type: :feature do
-  include ApplicationHelper
-
-  before do
-    @user = create :tsubasa
-  end
-
-  it 'profile display' do
-    visit user_path(@user)
-    expect(page).to have_selector 'h1', text: @user.name
-    expect(page).to have_title full_title(@user.name)
-    expect(page).to have_selector 'h1', text: @user.name
-    expect(page).to have_selector 'h1>img.gravatar'
-    expect(page).to have_content @user.microposts.count
-    @user.microposts.paginate(page: 1).each do |micropost|
-      expect(page).to have_content micropost.content
+  context 'user profile' do
+    include ApplicationHelper
+    let(:user) { create :tsubasa }
+    before do
+      visit user_path(user)
+    end
+    it 'profile display' do
+      expect(page).to have_selector 'h1', text: user.name
+      expect(page).to have_title full_title(user.name)
+      expect(page).to have_selector 'h1', text: user.name
+      expect(page).to have_selector 'h1>img.gravatar'
+      expect(page).to have_content user.microposts.count
+      user.microposts.paginate(page: 1).each do |micropost|
+        expect(page).to have_content micropost.content
+      end
     end
   end
 end
