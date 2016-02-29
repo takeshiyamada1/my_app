e77e68ae70beca099f131d7a2ca3414c732e42bf
ryuto <tun_tnd@i.softbank.jp>
2016-02-29 11:21:12 +0900

    fixes#35#36fixed user mailer

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,22 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
-  it 'account_activation' do
-    user = create :tsubasa
-    user.activation_token = User.new_token
-    mail = UserMailer.account_activation(user)
-    expect(mail).to have_attributes subject: 'Account activation', to: [user.email], from: ['from@example.com']
-    expect(mail.body.encoded).to match user.name
-    expect(mail.body.encoded).to match user.activation_token
-    expect(mail.body.encoded).to match CGI.escape(user.email)
+  context 'account' do
+    let(:user) { create :tsubasa }
+    let(:mail) { UserMailer.account_activation(user) }
+    before do
+      user.activation_token = User.new_token
+    end
+    it 'account_activation' do
+      expect(mail).to have_attributes subject: 'Account activation', to: [user.email], from: ['from@example.com']
+      expect(mail.body.encoded).to match user.name
+      expect(mail.body.encoded).to match user.activation_token
+      expect(mail.body.encoded).to match CGI.escape(user.email)
+    end
   end
 
-  it 'password_reset' do
-    user = create :tsubasa
-    user.reset_token = User.new_token
-    mail = UserMailer.password_reset(user)
-    expect(mail).to have_attributes subject: 'Password reset', to: [user.email], from: ['from@example.com']
-    expect(mail.body.encoded).to match user.reset_token
-    expect(mail.body.encoded).to match CGI.escape(user.email)
+  context 'password' do
+    let(:user) { create :tsubasa }
+    let(:mail) { UserMailer.password_reset(user) }
+    before do
+      user.reset_token = User.new_token
+    end
+    it 'password_reset' do
+      expect(mail).to have_attributes subject: 'Password reset', to: [user.email], from: ['from@example.com']
+      expect(mail.body.encoded).to match user.reset_token
+      expect(mail.body.encoded).to match CGI.escape(user.email)
+    end
   end
 end
