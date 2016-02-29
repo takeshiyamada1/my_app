8bca9978582257bf81fc5b155bb86d5f3aef0f2b
ryuto <tun_tnd@i.softbank.jp>
2016-02-29 15:56:33 +0900

    fixes#35fixed sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  it 'should ge new' do
-    get :new
-    expect(response).to have_http_status :success
+  context 'get new' do
+    before do
+      get :new
+    end
+    it 'should get new' do
+      expect(response).to have_http_status :success
+    end
   end
 end
