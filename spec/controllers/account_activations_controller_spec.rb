f7d2758eafb1c536f6645c180813621036067067
ryuto <tun_tnd@i.softbank.jp>
2016-03-02 15:59:37 +0900

    fixes#40fixed account activations

--- spec/controllers/account_activations_controller_spec.rb
+++ spec/controllers/account_activations_controller_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe AccountActivationsController, type: :controller do
-  let(:user) { create :tsubasa }
+  let(:user) { create :tsubasa, activated: false, activated_at: nil }
   context 'account active' do
     before do
       get :edit, id: user, email: user.email
-      log_in_as(user)
     end
     it 'should edit when account active' do
-      expect(user.activated?).to be_truthy
-      expect(flash).to_not be_empty
       expect(response).to redirect_to user_path(user)
+      expect(flash).to_not be_empty
+      expect(user.activated?).to be_truthy
+      expect(logged_in?).to be_truthy
     end
   end
 end
