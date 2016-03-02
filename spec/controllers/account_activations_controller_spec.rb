cecd3e7c28999155f7b9a3e0e59f32246ed9655d
ryuto <tun_tnd@i.softbank.jp>
2016-03-02 16:28:06 +0900

    fixes#40add account activations controller spec

--- spec/controllers/account_activations_controller_spec.rb
+++ spec/controllers/account_activations_controller_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe AccountActivationsController, type: :controller do
   let(:user) { create :tsubasa, activated: false, activated_at: nil }
   context 'account active' do
     before do
-      get :edit, id: user, email: user.email
+      get :edit, id: user.activation_token, email: user.email
     end
     it 'should edit when account active' do
       expect(response).to redirect_to user_path(user)
       expect(flash).to_not be_empty
-      expect(user.activated?).to be_truthy
+      expect(user.reload.activated).to be_truthy
       expect(logged_in?).to be_truthy
     end
   end
