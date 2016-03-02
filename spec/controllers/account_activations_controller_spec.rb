58ff1861d9b778e5cf749cf4c5e6a9c3ec3476e6
ryuto <tun_tnd@i.softbank.jp>
2016-03-02 17:48:31 +0900

    fixes#40fixed account activations controller

--- spec/controllers/account_activations_controller_spec.rb
+++ spec/controllers/account_activations_controller_spec.rb
@@ -39,6 +39,8 @@ RSpec.describe AccountActivationsController, type: :controller do
     it 'should edit when account active' do
       expect(response).to redirect_to user_path(user)
       expect(flash).to_not be_empty
+      binding.pry
+      expect(user.reload.activated_at).to be_present
       expect(user.reload.activated).to be_truthy
       expect(logged_in?).to be_truthy
     end
