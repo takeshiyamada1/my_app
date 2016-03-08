8366cf8c85ae257dc367c2dbf4b8209c40984a37
ryuto <tun_tnd@i.softbank.jp>
2016-03-08 09:50:50 +0900

    fixes#40fixed add microposts controller create/destroy

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -1,32 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe MicropostsController, type: :controller do
-
-  context 'create and destroy when not logged in' do
-    let!(:micropost) { create :orange }
-
-    it 'should redirect create when not logged in' do
-      expect { post :create, micropost: { content: 'Lorem ipsum' } }.to_not change { Micropost.count }
-      expect(response).to redirect_to login_url
+  let(:micropost) { create :orange }
+  let(:user) { create :tsubasa }
+  context 'create action' do
+    before do
+      micropost
     end
-
-    it 'should redirect destroy when not logged in' do
-      expect { delete :destroy, id: micropost }.to_not change { Micropost.count }
-      expect(response).to redirect_to login_url
+    context 'create micropost' do
+      before do
+        log_in_as(user)
+      end
+      it 'should redirec create micropost' do
+        expect { post :create, micropost: { content: 'micropost create', id: user } }.to change { Micropost.count}.by(1)
+        expect(flash).to be_present
+        expect(response).to redirect_to root_url
+      end
+    end
+    context 'when not logged in' do
+      it 'should redirect create when not logged in' do
+        expect { post :create, micropost: { content: 'Lorem ipsum' } }.to_not change { Micropost.count }
+        expect(response).to redirect_to login_url
+      end
+    end
+    context 'create not micropost' do
+      before do
+        log_in_as(user)
+      end
+      it 'should no create micropost' do
+        expect { post :create, micropost: { content: 'a'*141, id: user } }.to_not change  { Micropost.count }
+        expect(response).to render_template 'static_pages/home'
+      end
     end
   end
 
-  context 'wrong micropost destroy' do
-    let(:user) { build :tsubasa }
-    let!(:ants) { create :ants }
-    let!(:micropost) { create :orange }
-    before do
-      log_in_as(user)
+  context 'destroy action' do
+    context 'destroy micropost' do
+      let(:user) { create :tsubasa_with_microposts }
+      before do
+        log_in_as(user)
+      end
+      it 'should redirect destroy for wrong micropost' do
+        expect { delete :destroy, id: user }.to change { Micropost.count }.by(-1)
+        expect(response).to redirect_to root_url
+        expect(flash).to be_present
+      end
     end
 
-    it 'should redirect destroy for wrong micropost' do
-      expect { delete :destroy, id: ants }.to_not change { Micropost.count }
-      expect(response).to redirect_to root_url
+    context 'when not logged in' do
+      it 'should redirect destroy when not logged in' do
+        expect { delete :destroy, id: user }.to_not change { Micropost.count }
+        expect(response).to redirect_to login_url
+      end
+    end
+
+    context 'without micropost' do
+      let(:other) { create :mallory }
+      before do
+        log_in_as(other)
+      end
+      it 'should redirect destroy for wrong micropost' do
+        expect { delete :destroy, id: other }.to_not change { Micropost.count }
+        expect(response).to redirect_to root_url
+      end
     end
   end
 end
